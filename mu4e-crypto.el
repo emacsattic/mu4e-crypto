8eb4ce811044aa93f3527adf35984aa502ac0d4b
Merytamun <meritamen@sdf.org>
2024-01-20 14:46:41 +0800

    Update copyright years

--- mu4e-crypto.el
+++ mu4e-crypto.el
@@ -1,6 +1,6 @@
 ;;; mu4e-crypto.el --- Encrypt and decrypt emails for mu4e with GnuPG -*- lexical-binding: t -*-
 
-;; Copyright (C) 2023 Meritamen <meritamen@sdf.org>
+;; Copyright (C) 2023-2024 Meritamen <meritamen@sdf.org>
 
 ;; Author: Meritamen <meritamen@sdf.org>
 ;; URL: https://github.com/meritamen/mu4e-crypto
